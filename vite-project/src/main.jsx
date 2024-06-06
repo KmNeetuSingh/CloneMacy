9788db0fa5d26af08fe3e60dc24d1c2b20d82366
neetu singh <neetusingh99590@gamil.com>
2024-06-06 18:07:39 +0530

    setup

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,12 +1,13 @@
-import * as React from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
-import * as ReactDOM from 'react-dom/client'
+import * as React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import * as ReactDOM from 'react-dom/client';
+import App from './App.jsx'; 
 
-const rootElement = document.getElementById('root')
+const rootElement = document.getElementById('root');
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <ChakraProvider>
       <App />
     </ChakraProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
